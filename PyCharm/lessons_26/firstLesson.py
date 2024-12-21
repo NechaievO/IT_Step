5c5041367eecfd828f212c3d1a1c93a09d72a9ef
NechaievO <chuprin@seznam.cz>
2024-12-21 18:05:07 +0100

    add file pycharm

---
+++ PyCharm/lessons_26/firstLesson.py
@@ -0,0 +1,31 @@
+# print("Hello world!")
+from operator import truediv
+
+name = "Oleg"
+print(name)
+print ("Oleg")
+age = 50
+isStudent = True
+fruits = ["apple", "banana", "cherry"]
+print(fruits[0])
+numbers = [1, 2, 3, 4, 5]
+# numbers = [1, 2, 3, 4, 5]
+a = 5
+b = 3
+
+print("My name is", name)
+print("My name is"  +name)
+print("My name is {name}")
+
+print(a,b)
+print(a+b)
+print(f"{fruits[1]} = {numbers[4]}")
+student = {"name" : "Alice", "age" : 25, "is_student" : True}
+
+name = student["name"]
+print(name)
+age = student ["age"]
+print( student["age"])
+print(f"{name} student")
+
+
