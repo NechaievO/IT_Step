c6e061e3060a391cccea7347800afee304b118a8
NechaievO <chuprin@seznam.cz>
2025-01-23 19:58:33 +0100

    Homework3

---
+++ PyCharm/PythonHomeworks/homework3.py
@@ -0,0 +1,36 @@
+user_number = input ("Enter two-digit number: ")
+
+print(user_number[0])
+print(user_number[1])
+
+
+
+
+user_number = input ("Enter three-digit number: ")
+
+print(user_number[0])
+print(user_number[1])
+print(user_number[2])
+
+sum_of_digits = int(user_number[0]) + int(user_number[1]) + int(user_number[2])
+
+print( sum_of_digits)
+
+
+
+
+user_number1 = input("Enter first digit: ")
+user_number2 = input("Enter second digit: ")
+
+print (user_number1 + user_number2)
+
+
+
+user_temperature_Celsius = input("Enter temperature in Celsius: ")
+num = float(user_temperature_Celsius)
+temperature_Farenheit = (num * 9 / 5 + 32 )
+print("Temperature in Farenheit:", temperature_Farenheit)
+
+
+
+
